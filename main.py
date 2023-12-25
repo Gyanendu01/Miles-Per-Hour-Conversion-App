42242cd49e59fa787a7ac69db301218d3466e053
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-25 22:03:45 +0530

    Add exception handling for the code

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 print("\n\tWelcome to the Miles Per Hour Conversion App")
 
 def usr_input():
-    usr_name = input("\n\tEnter your username: ")
-    usr_speed=float(input("\n\tHello {}!\n\tWhat is your speed in miles per hour: ".format(usr_name)))
+    
+    try:
+        # Add user name and speed
+        usr_name = input("\n\tEnter your username: ")
+        usr_speed=float(input("\n\tHello {}!\n\tWhat is your speed in miles per hour: ".format(usr_name)))
+    except ValueError:
+        print("\n\tPlease enter correct input!!")
+    except Exception:
+        print("\n\tInvalid input!!")
     return usr_speed
 
 def speed_conversion(usr_speed):
+    # Logic to convert miles per hour to meters per second
     usr_speed = usr_speed * 0.4474
     print("\tYour speed in meters per second is {}.".format(round(usr_speed,2)))
 
